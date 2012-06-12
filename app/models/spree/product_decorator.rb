b2596d20d1d0cdf03bf5f54ea1d5aec1049fb3b6
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-12 17:45:47 +0200

    Added product class

---
+++ app/models/spree/product_decorator.rb
@@ -0,0 +1,4 @@
+Product.class_eval do
+  delegate_belongs_to :master, :tire_width_id, :tire_profile_id, :tire_innertube_id, :tire_ic_id, 
+                               :tire_speed_code_id, :tire_fr_id, :tire_tttl_id
+end
