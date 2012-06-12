22ec572d3d153d0161c89181d660cbf7164f6b52
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-12 18:58:58 +0200

    creating overrides

--- app/overrides/add_rodamoto_specs.rb
+++ app/overrides/add_rodamoto_specs.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => "spree/admin/products/form",
                      :name => "add_rodamoto_specs",
                      :insert_bottom => "#shipping_specs",
                      :text => "
-       <ul id="rodamoto_specs">
+       <ul>
         <li>
           <%= f.label :tire_speed_code_id, t(:speed_code) %>
           <%= f.collection_select(:tire_speed_code_id, Spree::TireSpeedCode.all, :id, :name, { :include_blank => true }, { 'style' => 'width:40px' }) %>
