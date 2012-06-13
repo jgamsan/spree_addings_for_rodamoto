b3dacb0f8fb79e5a9e069029a2a05efdf58f8160
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-06-13 11:45:50 +0200

    more overrides

--- app/overrides/add_rodamoto_specs.rb
+++ app/overrides/add_rodamoto_specs.rb
@@ -6,5 +6,13 @@ Deface::Override.new(:virtual_path => "spree/admin/products/_form",
         <li>
           <%= f.label :tire_speed_code_id, t(:speed_code) %>
           <%= f.collection_select(:tire_speed_code_id, Spree::TireSpeedCode.all, :id, :name, { :include_blank => true }, { 'style' => 'width:40px' }) %>
+        </li>
+        <li>
+          <%= f.label :tire_width_id, t(:width) %>
+          <%= f.collection_select(:tire_width_id, Spree::TireWidth.all, :id, :name, { :include_blank => true }, { 'style' => 'width:40px' }) %>
+        </li>
+        <li>
+          <%= f.label :tire_profile_id, t(:profile) %>
+          <%= f.collection_select(:tire_profile_id, Spree::TireProfile.all, :id, :name, { :include_blank => true }, { 'style' => 'width:40px' }) %>
         </li>  
        </ul>")
