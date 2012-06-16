711ae78b3b545449489c6ddaac0104b382404a37
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-16 17:59:47 +0200

    Accesible attributes

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Variant.class_eval do
+  attr_accessible :tire_fr_id, :tire_speed_code_id, :tire_tttl_id, :tire_profile_id, :tire_ic_id, :tire_innertube_id, :tire_width_id
   has_many :tire_widths
   has_many :tire_profiles
   has_many :tire_innertubes
