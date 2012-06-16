c9548bf1bd34cd87d003faeca90ada52a1b0bde6
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-16 09:55:14 +0200

    Added migration for load taxons in rodamoto

--- db/migrate/20120616074151_load_taxons_in_rodamoto.rb
+++ db/migrate/20120616074151_load_taxons_in_rodamoto.rb
@@ -2,11 +2,11 @@ class LoadTaxonsInRodamoto < ActiveRecord::Migration
   def up
     file = "#{Rails.root}/db/datas/taxons.csv"
     CSV.foreach(file, :col_sep =>',') do |row|
-      Spree::Taxon.create(:parent_id => row[0], :name => row[1], :taxonomy_id => [2])
+      Spree::Taxon.create!(:parent_id => row[0], :name => row[1], :taxonomy_id => [2])
     end
     file = "#{Rails.root}/db/datas/marcas.csv"
     CSV.foreach(file, :col_sep =>',') do |row|
-      Spree::Taxon.create(:parent_id => row[0], :name => row[1].capitalize, :taxonomy_id => [2])
+      Spree::Taxon.create!(:parent_id => row[0], :name => row[1].capitalize, :taxonomy_id => [2])
     end
   end
 
