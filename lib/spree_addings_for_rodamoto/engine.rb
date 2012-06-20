065d74649eaf183bb17c5f3084340e13944b5a5f
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-20 18:59:30 +0200

    Added Search class for rodamoto fields

--- lib/spree_addings_for_rodamoto/engine.rb
+++ lib/spree_addings_for_rodamoto/engine.rb
@@ -13,6 +13,9 @@ module SpreeAddingsForRodamoto
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      if Spree::Config.instance
+        Spree::Config.searcher_class = Spree::Search::Rodamoto
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
