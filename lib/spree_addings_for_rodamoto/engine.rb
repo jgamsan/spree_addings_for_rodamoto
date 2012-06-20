4d6b832cf66e65df6682f88b5f1f16f98dfca615
Jose Miguel Gamazo Sanchez <jose@jose-System-Product-Name.(none)>
2012-06-20 20:05:38 +0200

    remove Search from for tires

--- lib/spree_addings_for_rodamoto/engine.rb
+++ lib/spree_addings_for_rodamoto/engine.rb
@@ -13,7 +13,6 @@ module SpreeAddingsForRodamoto
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-      Spree::Config.searcher_class = Spree::Search::Rodamoto
     end
 
     config.to_prepare &method(:activate).to_proc
