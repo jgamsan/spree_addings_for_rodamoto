22f681c8a4b11643574a426ff2a97c5d782fe006
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-06-28 10:52:54 +0200

    mas en pvp

--- app/overrides/add_rodamoto_specs.rb
+++ app/overrides/add_rodamoto_specs.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "add_rodamoto_specs",
                      :insert_bottom => "#shipping_specs",
-                     :text => %q{ <%= render :partial => "spree/admin/products/_tires" %> })
+                     :text => %q{ <%= render :partial => "spree/admin/products/tires", :locals => {:f => f} %> })
