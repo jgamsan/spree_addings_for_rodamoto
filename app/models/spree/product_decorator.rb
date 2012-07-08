598bf1d8ed7e787c79dc4751b327312c7df5ba57
jose miguel <miguel.gamazo@galiclick.com>
2012-07-08 11:06:15 +0200

    Added accessible values

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,7 @@
 Spree::Product.class_eval do
-  attr_accessible :tire_fr_id, :tire_speed_code_id, :tire_tttl_id, :tire_profile_id, :tire_ic_id, :tire_innertube_id, :tire_width_id
+  attr_accessible :tire_fr_id, :tire_speed_code_id, :tire_tttl_id, :tire_profile_id, 
+  :tire_ic_id, :tire_innertube_id, :tire_width_id
+  attr_accessible :pvp3, :pvp7, :pvp9, :pvp12, :count_on_hand
   delegate_belongs_to :master, :tire_width_id, :tire_profile_id, :tire_innertube_id, :tire_ic_id, 
                                :tire_speed_code_id, :tire_fr_id, :tire_tttl_id
   delegate_belongs_to :master, :pvp3, :pvp7, :pvp9, :pvp12
