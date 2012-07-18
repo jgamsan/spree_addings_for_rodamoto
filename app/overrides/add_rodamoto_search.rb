aebb1e0edb8dfb43d3b35056cf6dbe8980753f10
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-07-18 17:07:12 +0200

    Mejoras en search partial

--- app/overrides/add_rodamoto_search.rb
+++ app/overrides/add_rodamoto_search.rb
@@ -3,5 +3,10 @@ Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
                      :insert_bottom => "li#search-bar",
                      :text => %q{
 <li id="tires-search-bar" data-hook>
-  <%= render :partial => 'spree/shared/search_tires' %>
+  <% unless session[:tires].nil? -%>
+    <%= form_tag products_path, :method => :get do -%>
+      <%= render :partial => 'spree/shared/search_tires' %>
+      <%= submit_tag t(:search), :name => nil %>
+  <% end -%>
+<% end -%>
 </li>})
